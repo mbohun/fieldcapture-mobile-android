9bced68061b7cf3b5064b24c73fbb08e30b56576
chris.godwin.ala <chris.godwin.ala@gmail.com>
2014-07-08 17:34:39 +1000

    Latest batch of activity forms.

--- GreenArmyMobile/src/main/assets/static/Ttgu9fflIR2OzMMsmkluFvyNHUlZnRJc1rPIIvryZ5N.js
+++ GreenArmyMobile/src/main/assets/static/Ttgu9fflIR2OzMMsmkluFvyNHUlZnRJc1rPIIvryZ5N.js
@@ -4408,17 +4408,28 @@ ko.vetoableObservable = function(initialValue, vetoCheck, noVetoCallback, vetoCa
  *
  */
 ko.bindingHandlers.popover = {
+
+    init: function(element, valueAccessor) {
+        ko.bindingHandlers.popover.initPopover(element, valueAccessor);
+    },
     update: function(element, valueAccessor) {
-        var options = ko.utils.unwrapObservable(valueAccessor());
 
-        var combinedOptions = ko.utils.extend({}, ko.bindingHandlers.popover.defaultOptions);
-        ko.utils.extend(combinedOptions, options);
+        var $element = $(element);
+        $element.popover('destroy');
+        var options = ko.bindingHandlers.popover.initPopover(element, valueAccessor);
+        if (options.autoShow) {
+            if ($element.data('firstPopover') === false) {
+                $element.popover('show');
+                $('body').on('click', function(e) {
 
-        $(element).popover(combinedOptions);
+                    if (e.target != element && $element.find(e.target).length == 0) {
+                        $element.popover('hide');
+                    }
+                });
+            }
+            $element.data('firstPopover', false);
+        }
 
-        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-            $(element).popover("destroy");
-        });
     },
 
     defaultOptions: {
@@ -4426,6 +4437,22 @@ ko.bindingHandlers.popover = {
         animation: true,
         html: true,
         trigger: "hover"
+    },
+
+    initPopover: function(element, valueAccessor) {
+        var options = ko.utils.unwrapObservable(valueAccessor());
+
+        var combinedOptions = ko.utils.extend({}, ko.bindingHandlers.popover.defaultOptions);
+        var content = ko.utils.unwrapObservable(options.content);
+        ko.utils.extend(combinedOptions, options);
+        combinedOptions.description = content;
+
+        $(element).popover(combinedOptions);
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            $(element).popover("destroy");
+        });
+        return options;
     }
 };
 
